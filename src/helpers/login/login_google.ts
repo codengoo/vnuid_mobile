8eb74e3265f67f7531411bb7fc9dae57c1f024eb
NghiaDT <nghiadt22.work@gmail.com>
2025-04-14 00:23:24 +0700

    update

--- src/helpers/login/login_google.ts
+++ src/helpers/login/login_google.ts
@@ -1,7 +1,9 @@
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
-import { TFunction } from 'i18next';
-import {useTranslation} from 'react-i18next';
+import {TFunction} from 'i18next';
+import DeviceInfo from 'react-native-device-info';
 import Toast from 'react-native-toast-message';
+import { axios } from '../network';
+import * as Keychain from 'react-native-keychain';
 
 GoogleSignin.configure({
   webClientId:
@@ -13,6 +15,7 @@ async function loginGoogle(): Promise<string | undefined> {
   try {
     await GoogleSignin.hasPlayServices();
     const userInfo = await GoogleSignin.signIn();
+
     return userInfo.data?.idToken || void 0;
   } catch (error) {
     console.error('Google Sign-In Error:', error);
@@ -20,15 +23,39 @@ async function loginGoogle(): Promise<string | undefined> {
   }
 }
 
-export async function signInWithGoogle(t:  TFunction<"login", undefined>) {
+function toastFailed(t: TFunction<'login', undefined>) {
+  Toast.show({
+    type: 'error',
+    text1: t('toast_failed'),
+    text2: t('toast_failed_description'),
+    autoHide: true,
+  });
+}
+
+export async function signInWithGoogle(t: TFunction<'login', undefined>) {
   const idToken = await loginGoogle();
-  
-  if (!idToken) {
-    Toast.show({
-      type: 'error',
-      text1: t('toast_failed'),
-      text2: t('toast_failed_description'),
-      autoHide: false,
+  if (!idToken) toastFailed(t);
+
+  const deviceID = await DeviceInfo.getUniqueId();
+  const deviceName = await DeviceInfo.getDeviceName();
+
+  try {    
+    const response = await axios.post('/auth/login_google', {
+      id_token: idToken,
+      device_id: deviceID,
+      device_name: deviceName,
     });
+    console.log(response.data);
+
+    if (response.status === 200) {
+      // save token
+      await Keychain.setGenericPassword('authToken', response.data.token);
+      // move to main screen
+    } else {
+      // save tmp token and move to 2fa screen
+    }
+  } catch (error) {
+    console.log((error as Error).message);
+    toastFailed(t);
   }
 }
---
+++ src/helpers/network/axios.ts
@@ -0,0 +1,6 @@
+import {default as _axios} from 'axios';
+
+export const axios = _axios.create({
+  // url: 'http://localhost:4444',
+  baseURL: 'http://10.0.2.2:4444',
+});
